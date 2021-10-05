781e0396aef102f536a0dbb00adca183d076d868
Paul Conway <conwaypauliii@gmail.com>
2021-10-05 00:02:26 -0400

    Fixed bug preventing using old json

--- test_data_gen/__main__.py
+++ test_data_gen/__main__.py
@@ -18,8 +18,8 @@ def main():
     # TODO: Implement switch case to allow the user to select from a range of options, such as generate JSON, generate test data, etc.
     # Note: Switch cases are being implemented in Python 3.10, which is going to be released October 4th, 2021
     print(__doc__)
-    is_new = None
-    while is_new == None:  # TODO: exception handling
+    file_state = False
+    while file_state == False:  # TODO: exception handling
         json_name = input(
             "Specify the name of a JSON file (without extension) you would like to read from.\nIf no such file exists, you will be prompted if you would like to create it:\n"
         )
@@ -27,20 +27,18 @@ def main():
         json_path = os.path.join(
             os.path.dirname(os.path.realpath(__file__)), "local_data", "json", json_name
         )
-        print(str(json_path))
         if os.path.isfile(json_path) == False:
             pref_new = input(
                 json_name + " does not exist, would you like to create it? (y/n)\n"
             )
             if pref_new.lower() == "y":
                 core.json_gen.generate(json_name, json_path)
-                is_new = True
+                file_state = True
             else:
-                is_new == None
+                file_state == False
         else:
-            core.json_gen.generate(json_name, json_path)
-            is_new = False
-        core.pos_gen.generate(json_path)
+            file_state = True
+    core.pos_gen.generate(json_path)
 
 
 if __name__ == "__main__":
