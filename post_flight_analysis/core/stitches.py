ba7647856c858d87d93eac13f4fcbe69f5aaf55d
Paul <conwayp2@my.erau.edu>
2022-02-17 21:40:48 -0500

    independant variable is now spearate array

--- post_flight_analysis/core/stitches.py
+++ post_flight_analysis/core/stitches.py
@@ -2,11 +2,12 @@
 TODO: Docstring
 """
 
-def stitch(low,high,threshold,dim=3):
+def stitch(time,low,high,threshold,dim=3):
     """
     Inputs:
-        low ................... numpy array [x,y_1,y_2,...,y_n]
-        high .................. numpy array [x,y_1,y_2,...,y_n]
+        time .................. numpy array [x]
+        low ................... numpy array [y_1,y_2,...,y_n]
+        high .................. numpy array [y_1,y_2,...,y_n]
         threshold ............. float value where low values will be replaced
         dim (opt, defalt=3) ... number of dependant variables (y-values)
     Returns:
