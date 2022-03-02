4fec7d0c473c1d84a13f123943edba5d70f5d155
Eric Yoerg <yoergacc@gmail.com>
2022-03-01 20:51:38 -0500

    Completed pressure to pressure altitude converter

--- post_flight_analysis/core/ISA_Altitude_gen.py
+++ post_flight_analysis/core/ISA_Altitude_gen.py
@@ -3,50 +3,41 @@ ISA Altitude Analysis
 Version: 1.0.0
 Author(s): Alijah McDonald, Eric Yoerg
 
-Description: This file imports generated barometric pressure data from a .csv file
-and plots ISA altitude based on a pressure --> ISA Altitude conversion.
+Description: This file imports generated barometric pressure data from numpy array and converts
+pressure to pressure altitude, returning the data to pressure_alt.
 """
 
 import numpy as np
-from matplotlib import pyplot as plt 
 import pandas as pd
 from pandas import DataFrame
 import os
-import csv
 
 def ISA_altitude(pressure):
-    #define csv path to pull csv from
-    csv_path = input('Please input the csv file path you would like to read:') 
-    
-    #import csv file
-    #modify to input a single numpy array
-    with open(csv_path, mode='r') as csv_file:
-        #read csv that has specified input path
-        df = pd.read_csv(csv_path)
 
-        #create matrices of time and pressure values
-        time, pressure = df["time"], df["pressure"]
-
-    T_o = 518.4 #Mean sea level temperature rankine
-    B = 0.00357 #lapse rate
+    #Define
+    T_o = 288.15 #Mean sea level temperature rankine
+    B = 0.0065 #lapse rate
     P_o = 101325 #Mean Sea level pressure
+    R = 287.1
+    g = 9.81
+    alt = np.zeros((len(pressure),))
+    print(str(alt))
 
-    #Convert pressure to pressure altitude and store inside altitude
+    #Convert pressure to pressure altitude and store inside Pressure_alt
     for x in range(len(pressure)):
-        pressure[x] = ((T_o/B)*(1-((int(pressure[x])/P_o)**0.1903)))*0.3048
-
-    print(pressure)
-    #Write pressure altitude and time data to csv
-    #change the file to pandas writer and make it so that the path is editable
-    with open('C:\Ground-Station\post_flight_analysis\proc_data\Altitude.csv',mode='w') as alt_csv:
+        alt[x] = (T_o/B)*(1-((pressure[x]/P_o)**((R*B)/g)))
+        #print(str(pressure))
         
-        #Create the csv writer
-        writer = csv.writer(alt_csv)
-
-        #Write time and pressure altitude
-        writer.writerows(time)
-        writer.writerows(pressure)
+    return alt
 
 
-    #plt.plot(time,altitude)
-    #plt.show()
+
+
+
+
+
+    
+
+
+
+
