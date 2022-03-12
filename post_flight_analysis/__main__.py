b375b74af1524d07ba28b671ea9abeadceacad3a
Eric Yoerg <yoergacc@gmail.com>
2022-03-11 20:10:36 -0500

    Added Plots and formatting to main file

--- post_flight_analysis/__main__.py
+++ post_flight_analysis/__main__.py
@@ -5,6 +5,7 @@ TODO: Docstring
 # Formatted with Black, the uncompromising Python code formatter.
 
 # Imports
+import graphlib
 import matplotlib.pyplot as plt
 
 import numpy as np
@@ -59,8 +60,8 @@ def main():
     pos_y = core.integrate.left_sum(time, vel[1])
     pos_z = core.integrate.left_sum(time, vel[2])
     pos = np.asarray([pos_x, pos_y, pos_z])
+    xy_pos = [np.sqrt(pos[0][i]**2 + pos[1][i]**2) for i,_ in enumerate(time)]
     pos_mag = [np.sqrt(pos[0][i]**2 + pos[1][i]**2 + pos[2][i]**2) for i,_ in enumerate(time)]
-
 
     force = core.force_calc.calculate_force(time, accel, m_i, m_f, burn_time)
     force_mag = [np.sqrt(force[0][i]**2 + force[1][i]**2 + force[2][i]**2) for i,_ in enumerate(time)]
@@ -72,17 +73,60 @@ def main():
     # TODO: Calculate ISA and accel calculated altitude divergence
 
     # Plottting happens here:
+
+    #Acceleration (x, y, x, mag w.r.t. t)
     plt.subplot(3,3,1)
     plt.plot(time,accel[0])
     plt.plot(time,accel[1])
     plt.plot(time,accel[2])
     plt.plot(time,accel_mag)
     plt.legend(["x","y","z","net"])
-    plt.title("Acceleration, m/s")
+    plt.title("Acceleration, m/s^2")
 
+    #Down Range (x, y, mag w.r.t. t)
     plt.subplot(3,3,2)
+    plt.plot(time,pos_x)
+    plt.plot(time,pos_y)
+    plt.plot(time,xy_pos)
+    plt.legend(["x","y","net"])
+    plt.title("Down Range, m")
 
+    #Velocity (x, y, z, mag w.r.t. t)
+    plt.subplot(3,3,4)
+    plt.plot(time,vel_x)
+    plt.plot(time,vel_y)
+    plt.plot(time,vel_z)
+    plt.plot(time,vel_mag)
+    plt.legend(["x","y","z","net"])
+    plt.title("Velocity, m/s")
 
+    #Altitude (from Pressure and acceleration w.r.t. t)
+    plt.subplot(3,3,5)
+    plt.plot(time,pos_z)
+    plt.plot(time,pressure_alt)
+    plt.legend(["accel_alt, pressure_alt"])
+    plt.title("Altitude, m")
+
+    #Position (x, y, z w.r.t. t)
+    plt.subplot(3,3,7)
+    plt.plot(time,pos_x)
+    plt.plot(time,pos_y)
+    plt.plot(time,pos_z)
+    plt.plot(time,xy_pos)
+    plt.legend(["x","y","z","net"])
+    plt.title("Position, m")
+
+    #Force (x, y, z w.r.t. t)
+    plt.subplot(3,3,8)
+    plt.plot(time,force[0])
+    plt.plot(time,force[1])
+    plt.plot(time,force[2])
+    plt.plot(time,force_mag)
+    plt.legend(["x","y","z","net"])
+    plt.title("Force, m")
+
+    #Plot Formatting
+    plt.tight_layout(pad = 0.005)
     plt.show()
     return None
 
