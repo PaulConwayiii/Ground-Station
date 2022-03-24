Fixed intermediate angle calculating from omega in Rotation function

post_flight_analysis/core/rotation.py
@@ -1,4 +1,5 @@
 import numpy as np
+from scipy.spatial.transform import Rotation as R
 
 
 def rotate(omega, step, R_frame, nose_axis="z"):
@@ -51,11 +52,16 @@ def rotate(omega, step, R_frame, nose_axis="z"):
     RR_y = np.zeros((len(step),))
     RR_z = np.zeros((len(step),))
 
+    delta_t = step[1] - step[0]
+
     # These are not really Euler angles (unless it's a special case where they happen to be)
     for n, s in enumerate(step):
-        alpha = omega[0][n] * s
-        beta = omega[1][n] * s
-        gamma = omega[2][n] * s
+
+        alpha = omega[0][n] * delta_t
+        beta = omega[1][n] * delta_t
+        gamma = omega[2][n] * delta_t
+
+        # roted = np.sqrt(alpha**2 + beta**2 + gamma**2) * deta_t
 
         # It's faster to do this once rather than compute these trig functions 12 times
         c_alpha = np.cos(alpha)
@@ -72,6 +78,8 @@ def rotate(omega, step, R_frame, nose_axis="z"):
         Q_3 = np.array([[c_gamma, -s_gamma, 0], [s_gamma, c_gamma, 0], [0, 0, 1]])
         # This matrix combines all rotations
         Q = np.matmul(np.matmul(Q_1, Q_2), Q_3)
+
+        # quat = R.as_quat()
 
         # Rotated vector
         raw_vec = [R_frame[0][n], R_frame[1][n], R_frame[2][n]]

test_data_gen/core/rotation.py
@@ -51,11 +51,13 @@ def rotate(omega, step, R_frame, nose_axis="z"):
     RR_y = np.zeros((len(step),))
     RR_z = np.zeros((len(step),))
 
+    delta_t = step[1] - step[0]
+
     # These are not really Euler angles (unless it's a special case where they happen to be)
     for n, s in enumerate(step):
-        alpha = omega[0][n] * s
-        beta = omega[1][n] * s
-        gamma = omega[2][n] * s
+        alpha = omega[0][n] * delta_t
+        beta = omega[1][n] * delta_t
+        gamma = omega[2][n] * delta_t
 
         # It's faster to do this once rather than compute these trig functions 12 times
         c_alpha = np.cos(alpha)